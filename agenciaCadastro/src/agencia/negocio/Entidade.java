726d928cea1c576ce3f7f3f4c2e85109fea92a58
Taysonj0 <taysontavarestf@gmail.com>
2025-09-04 17:44:25 -0300

    Update Entidade.java

--- agenciaCadastro/src/agencia/negocio/Entidade.java
+++ agenciaCadastro/src/agencia/negocio/Entidade.java
@@ -45,7 +45,11 @@ public abstract class Entidade {
 	public void setCidade(String cidade) {
 		this.cidade = cidade;
 	}
+<<<<<<< Updated upstream
 
+=======
+	
+>>>>>>> Stashed changes
     public String getSexo() {
 		return sexo;
 	}
@@ -53,9 +57,12 @@ public abstract class Entidade {
 	public void setSexo(String sexo) {
 		this.sexo = sexo;
 	}
+<<<<<<< Updated upstream
 	
 	// Verifica se duas entidades são iguais (baseado no nome, habilidades e sexo)
 	// Retorna true se as entidades forem iguais
+=======
+>>>>>>> Stashed changes
 
 	@Override
     public boolean equals(Object obj){
